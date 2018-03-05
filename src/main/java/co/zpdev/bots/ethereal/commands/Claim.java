e11d34fcde3f9404dc04fc5d2c9a87c74d561a78
ZP4RKER <zp4rker@programmer.net>
2018-03-05 14:19:03 +1100

    added claim log

--- src/main/java/co/zpdev/bots/ethereal/commands/Claim.java
+++ src/main/java/co/zpdev/bots/ethereal/commands/Claim.java
@@ -2,7 +2,12 @@ package co.zpdev.bots.ethereal.commands;
 
 import co.zpdev.bots.core.command.Command;
 import co.zpdev.bots.ethereal.Ethereal;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.time.Instant;
 
 public class Claim {
 
@@ -12,10 +17,17 @@ public class Claim {
         message.delete().queue();
 
         if (message.getMember().getRoles().stream().noneMatch(r -> r.getIdLong() == Ethereal.SR)) return;
-        if (message.getTextChannel().getTopic().equals("Unclaimed")) return;
+        if (!message.getTextChannel().getTopic().equals("Unclaimed")) return;
 
         String name = message.getAuthor().getName() + "#" + message.getAuthor().getDiscriminator();
         message.getTextChannel().getManager().setTopic("Claimed by " + name).queue();
+
+        TextChannel logs = message.getGuild().getTextChannelById(Ethereal.LOGS);
+        MessageEmbed log = new EmbedBuilder().setColor(Ethereal.EMBED)
+                .setAuthor("Ticket Claimed")
+                .setDescription("#" + message.getChannel().getName() + " claimed by **" + message.getMember().getEffectiveName() + "**")
+                .setTimestamp(Instant.now()).build();
+        logs.sendMessage(log).queue();
     }
 
 }
