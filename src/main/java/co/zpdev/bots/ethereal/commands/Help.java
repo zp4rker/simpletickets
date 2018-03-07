1c076686070721d47c6aad41ab01ec05be2845ac
ZP4RKER <zp4rker@programmer.net>
2018-03-07 22:59:03 +1100

    updated help command

--- src/main/java/co/zpdev/bots/ethereal/commands/Help.java
+++ src/main/java/co/zpdev/bots/ethereal/commands/Help.java
@@ -14,7 +14,10 @@ public class Help {
 
         MessageEmbed embed = new EmbedBuilder().setColor(Ethereal.EMBED)
                 .setAuthor("Ethereal Bot Commands", null, message.getJDA().getSelfUser().getEffectiveAvatarUrl())
-                .setDescription("**-new** Creates a ticket\n**-add @user #ticket** Adds a member to a ticket\n**-claim** Claims a ticket")
+                .setDescription("**-new** Creates a ticket\n" +
+                        "**-add @user #ticket** Adds a member to a ticket\n" +
+                        "**-claim** Claims a ticket\n" +
+                        "**-status <aip|ip|afp|c>** Sets the status of a ticket")
                 .setFooter("Made by ZP4RKER#3333", null).build();
 
         message.getChannel().sendMessage(embed).queue();
