f6b0f1a14fa667b15a35868765ba9c7e1fcb3384
ZP4RKER <zp4rker@programmer.net>
2018-03-07 22:41:27 +1100

    testing

--- src/main/java/co/zpdev/bots/ethereal/commands/Status.java
+++ src/main/java/co/zpdev/bots/ethereal/commands/Status.java
@@ -15,7 +15,7 @@ public class Status {
         if (message.getMember().getRoles().stream().noneMatch(r -> r.getIdLong() == Ethereal.SR)) return;
         if (!message.getMentionedChannels().get(0).getName().startsWith("ticket-")) return;
         if (args.length != 1 || !args[0].matches("(?i:aip|ip|afp|c)")) return;
-        if (message.getTextChannel().getTopic().split(" \\| ").length < 2) return;
+        //if (message.getTextChannel().getTopic().split(" \\| ").length < 2) return;
 
         String salesRep = message.getTextChannel().getTopic().split(" \\| ")[0];
         String status;
